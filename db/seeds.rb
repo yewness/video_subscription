0a0cd7c6c4d75fd94809acdf44f5287a764dbf0d
Yew Hong <yewhong_ng91@hotmail.com>
2016-02-04 18:38:00 +0800

    Deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ User.create(email: "dev@gmail.com", password: "123456789", level: 3)
 
 Video.create(link: "SASKwGrusaU", title: "Standard 1 Lesson 1-1", description: "Understanding whole numbers", category_id: 1)
 Video.create(link: "j5PwuW6TH2q4", title: "Standard 5 Lesson 1-3", description: "Addition with the highest total of 1000000", category_id: 2)
-Video.create(link: "lUIp6-m_zVM", title: "Standard 1 Lesson 1-4", description: "Subtraction within the range of 1000000", , category_id: 2)
+Video.create(link: "lUIp6-m_zVM", title: "Standard 1 Lesson 1-4", description: "Subtraction within the range of 1000000", category_id: 2)
 Video.create(link: "yfRXSpMXyBo", title: "Standard 5 Lesson 1-5", description: "Multiplication with the highest product of 1000000", category_id: 2)
 Video.create(link: "jntaHfDPYvQ", title: "Standard 5 Lesson 1-6", description: "Division with the highest dividend of 1000000", category_id: 2)
 
